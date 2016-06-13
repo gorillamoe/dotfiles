66164548f0150d8031003771708529799f81e3f2
Marco Kellershoff <marco.kellershoff@gofeminin.de>
2016-06-13 15:53:46 +0200

    Tweak argument handling

--- bin/check-inet-iface-up-down.sh
+++ bin/check-inet-iface-up-down.sh
@@ -1,22 +1,54 @@
 #!/bin/bash
-IFACE="enp3s0"
+
+# Bash Arguments Parsing
+# Use > 1 to consume two arguments per pass in the loop (e.g. each
+# argument has a corresponding value to go with it).
+# Use > 0 to consume one or more arguments per pass in the loop (e.g.
+# some arguments don't have a corresponding value to go with it such
+# as in the --default example).
+# note: if this is set to > 0 the /etc/hosts part is not recognized ( may be a bug )
+# See: http://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash#answer-14203146
+
+# Default values
+DEVICE="enp3s0"
 INTERVAL=10
 
-if [ ! -z $1 ]; then IFACE=$1; fi
-if [ ! -z $2 ]; then INTERVAL=$2; fi
+# Overrides
+while [[ $# > 1 ]]; do
+  key="$1"
 
-echo "Starting check for $IFACE; checking every $INTERVAL seconds if internet is up."
+  case $key in
+    -d|--device)
+    DEVICE="$2"
+    shift
+    ;;
+    -i|--interval)
+    INTERVAL="$2"
+    shift
+    ;;
+    --default)
+    DEFAULT=YES
+    ;;
+    *)
+      # unknown option
+    ;;
+  esac
+  shift
+done
+
+echo -e "\nStarting check for \e[1m$DEVICE\e[0m.\nChecking every \e[1m$INTERVAL\e[0m seconds if internet is up."
 
 while ( true ); do
-  wget -q --spider http://google.com
+
+  wget -q --spider "https://www.google.com/"
 
   if [ $? -ne 0 ]; then
-    echo ""
-    echo -e "Internet \e[31mis down.\e[39m Resetting iface now..."
-    sudo ip link set $IFACE down && sudo ip link set enp3s0 up
-    echo ""
-  else
-    echo -ne "."
+
+    echo -e "\n\e[1m" $(date) "\e[0mInternet \e[31mis down\e[39m. Resetting $DEVICE now...\n"
+    sudo ip link set $DEVICE down && sudo ip link set $DEVICE up
+
   fi
+
   sleep $INTERVAL
+
 done
