7ad76691196af20f1bf1534002465ab842146132
Marco Kellershoff <marco.kellershoff@gofeminin.de>
2016-06-13 15:30:04 +0200

    Add interval option and make output colored..

--- bin/check-inet-iface-up-down.sh
+++ bin/check-inet-iface-up-down.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 IFACE="enp3s0"
+INTERVAL=10
 
-if [ ! -z $1 ]; then
-  IFACE=$1
-fi
+if [ ! -z $1 ]; then IFACE=$1; fi
+if [ ! -z $2 ]; then INTERVAL=$2; fi
 
-echo "Starting check for $IFACE"
+echo "Starting check for $IFACE; checking every $INTERVAL seconds if internet is up."
 
 while ( true ); do
   wget -q --spider http://google.com
 
   if [ $? -ne 0 ]; then
     echo ""
-    echo "Internet is down. Resetting iface now..."
+    echo -e "Internet \e[31mis down.\e[39m Resetting iface now..."
     sudo ip link set $IFACE down && sudo ip link set enp3s0 up
     echo ""
   else
     echo -ne "."
   fi
-  sleep 5
+  sleep $INTERVAL
 done
