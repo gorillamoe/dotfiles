b5d2ff5a00f07646bdaf574f64d2903ead15309e
Marco Kellershoff <marco.kellershoff@gofeminin.de>
2016-08-03 11:44:23 +0200

    Add usage info

--- bin/md5sum-file-check.bash
+++ bin/md5sum-file-check.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+usage(){
+  echo "Usage: ./md5sum-file-check.bash [FILENAME] [MD5SUM]"
+  exit $1
+}
 run(){
   local RED='\033[0;31m'
   local GREEN='\033[0;32m'
@@ -10,5 +14,12 @@ run(){
   else
     printf "${RED}[ NOK ]${NC}\n"
   fi
+  exit 0
 }
+if [ -z $1 ]; then
+  usage 1
+fi
+if [ -z $2 ]; then
+  usage 2
+fi
 run $1 $2
