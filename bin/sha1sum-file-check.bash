3cdef4272373c819e533c0b0bf54c35b9432a68e
Marco Kellershoff <marco@kellershoff.net>
2017-05-09 10:06:20 +0200

    Add helper for checking sha1 file checksums

---
+++ bin/sha1sum-file-check.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+usage(){
+  echo "Usage: ./md5sum-file-check.bash [FILENAME] [SHA1SUM]"
+  exit $1
+}
+run(){
+  local RED='\033[0;31m'
+  local GREEN='\033[0;32m'
+  local NC='\033[0m' # No Color
+  local CHECKSUM=`sha1sum $1 | awk '{print $1}'`
+  if [ $CHECKSUM == $2 ]; then
+    printf "${GREEN}[ OK ]${NC}\n"
+  else
+    printf "${RED}[ NOK ]${NC}\n"
+  fi
+  exit 0
+}
+if [ -z $1 ]; then
+  usage 1
+fi
+if [ -z $2 ]; then
+  usage 2
+fi
+run $1 $2
