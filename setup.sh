ae741af1518647be69bf9fc1b28d56a795e517d2
Marco Kellershoff <marco@kellershoff.net>
2017-05-09 10:04:09 +0200

    Update name in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 
 # Simple Symlink-Script by Marco Kellershoff
-# marcokellershoff.com
-# babbafresh.com
+# emkae.net
+# mnmlst.org
 # Feel free to grab it and modify it without any restrictions!
 
 EXCLUDED_FILES=("." ".." ".git" ".gitignore" "setup.sh" "readme.markdown")
