fbe6591eb2b01d74b3e70ae46fc5ccfdda88d2a3
Marco Kellershoff <marco@kellershoff.net>
2017-07-05 09:47:31 +0200

    Modified yt2mp3 script to work more reliable

--- bin/yt2mp3.sh
+++ bin/yt2mp3.sh
@@ -1,18 +1,23 @@
-#!/bin/bash 
-# Script for grabbing the audio of youtube videos as mp3. 
-address=$1
-bitrate=$2
-default_bitrate=256
-title=$(youtube-dl --get-title $address)
-filename=$(youtube-dl --get-filename $address)
-youtube-dl $address
-if [[ $? -ge 0 ]]; then
-  ffmpeg -i "$filename" "$filename".wav 
-  if [[ -z $bitrate ]]; then 
-    bitrate=$default_bitrate
-  fi 
-  lame -b $bitrate "$filename".wav "$title".mp3 
-  rm "$filename" "$filename".wav 
-else
-  echo Fehler: Es konnte keine mp3-Datei erstellt werden.
-fi
+#!/bin/bash
+set -euo pipefail
+
+# Script for grabbing the audio of youtube videos as mp3.
+# -------------------------------------------------------
+#
+# Example:
+# 
+# yt2mp3.sh https://www.youtube.com/watch?v=KdV5J6WzFJQ
+
+main() {
+  address="$1"
+  filename=$(youtube-dl --get-filename "$address")
+
+  if youtube-dl "$address"; then
+    ffmpeg -i "$filename" "$filename".mp3
+  else
+    echo Fehler: Es konnte keine mp3-Datei erstellt werden.
+  fi
+}
+
+main "$1"
+
