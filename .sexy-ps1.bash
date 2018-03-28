7740aa81918c6786c3c336c350817be10b6fa223
Marco Kellershoff <marco.kellershoff@gmail.com>
2018-03-28 18:29:10 +0200

    Change \w to \W in PS1

--- .sexy-ps1.bash
+++ .sexy-ps1.bash
@@ -24,6 +24,6 @@ function color_my_prompt {
         #         git_icon=$fancyx
         # fi
         # git_branch='`git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\ \(\\\\\1\)\ /`'
-        export PS1="\[$color_red\]\u\[$color_yellow\]@\[$color_blue\]\h\[$color_cyan2\]:\[$color_cyan\]\w\[$color_green\]\$\[$reset\] "
+        export PS1="\[$color_red\]\u\[$color_yellow\]@\[$color_blue\]\h\[$color_cyan2\]:\[$color_cyan\]\W\[$color_green\]\$\[$reset\] "
 }
 color_my_prompt
