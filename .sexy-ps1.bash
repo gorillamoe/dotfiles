da0da335f98488843e5da3b049c39ed9437b8f1b
Marco Kellershoff <marco.kellershoff@gmail.com>
2018-03-28 18:19:11 +0200

    Fix mangled line-wrap through bad PS1 export

--- .sexy-ps1.bash
+++ .sexy-ps1.bash
@@ -3,15 +3,14 @@ function color_my_prompt {
         local fancyx='\342\234\227'
         # unicode "✓"
         local fancycheckmark='\342\234\223'
-        local color_red="$(tput setaf 1)"
-        local color_green="$(tput setaf 2)"
-        local color_yellow="$(tput setaf 3)"
-        local color_blue="$(tput setaf 4)"
-        local color_cyan="$(tput setaf 5)"
-        local color_brightblue="$(tput setaf 6)"
-        local color_grey="$(tput setaf 7)"
-        local last_color="\[\033[00m\]"
-        local defaults_text="$color_red\u$color_yellow@$color_blue\h$color_cyan2:$color_cyan\W"
+        local color_red=$(tput setaf 1)
+        local color_green=$(tput setaf 2)
+        local color_yellow=$(tput setaf 3)
+        local color_blue=$(tput setaf 4)
+        local color_cyan=$(tput setaf 5)
+        local color_brightblue=$(tput setaf 6)
+        local color_grey=$(tput setaf 7)
+        local reset=$(tput sgr 0)
         # local git_icon=""
         # local git_branch_color=""
         # local git_branch=""
@@ -25,6 +24,6 @@ function color_my_prompt {
         #         git_icon=$fancyx
         # fi
         # git_branch='`git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\ \(\\\\\1\)\ /`'
-        export PS1="$defaults_text$color_green\$$last_color "
+        export PS1="\[$color_red\]\u\[$color_yellow\]@\[$color_blue\]\h\[$color_cyan2\]:\[$color_cyan\]\w\[$color_green\]\$\[$reset\] "
 }
 color_my_prompt
