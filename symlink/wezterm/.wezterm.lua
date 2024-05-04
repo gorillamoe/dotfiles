ebbb5077af79eb6baec5a0665ab4462e1dde0928
Marco Kellershoff <marco@kellershoff.net>
2024-05-04 19:32:35 +0200

    Add Noto Color Emoji to wezterm fonts

--- symlink/wezterm/.wezterm.lua
+++ symlink/wezterm/.wezterm.lua
@@ -3,7 +3,12 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'BlulocoDark'
-config.font = wezterm.font("Fira Code Nerd Font")
+config.font = wezterm.font_with_fallback({
+  "Fira Code Nerd Font",
+  "Noto Color Emoji",
+})
+
+
 config.font_size = 12.0
 config.enable_wayland = true
 
