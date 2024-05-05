f57b767549af156259ef2f8055ac1b680963d45a
Marco Kellershoff <marco@kellershoff.net>
2024-05-05 12:29:13 +0200

    ✨ Add , && ,, aliases

--- symlink/oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
+++ symlink/oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
@@ -16,3 +16,12 @@ fi
 if command -v fdfind &> /dev/null; then
   alias 'fd'='fdfind'
 fi
+
+alias ","="cd \$(fd --type d --hidden --exclude \'.git\' --exclude \'.npm\' | fzf-tmux -p)"
+,,() {
+  local r=$(fd --type f --hidden --exclude '.git' --exclude '.npm' | fzf-tmux -p)
+  local dn=$(dirname $r)
+  local fn=$(realpath -s --relative-to=$dn $r)
+  cd $dn
+  nvim $fn
+}
