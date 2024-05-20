852fdb5550aafcafd1eb76935be9f1a050498ca4
Marco Kellershoff <marco@kellershoff.net>
2024-05-20 13:22:09 +0200

    feat: make wezterm font config awesome 🎉

--- symlink/wezterm/.wezterm.lua
+++ symlink/wezterm/.wezterm.lua
@@ -3,11 +3,34 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'BlulocoDark'
+
 config.font = wezterm.font_with_fallback({
   "Fira Code Nerd Font",
   "Noto Color Emoji",
 })
 
+config.font_rules = {
+  {
+    italic = false,
+    intensity = "Normal",
+    font = wezterm.font("Fira Code Nerd Font"),
+  },
+  {
+    italic = false,
+    intensity = "Bold",
+    font = wezterm.font("Fira Code Nerd Font"),
+  },
+  {
+    italic = true,
+    intensity = "Normal",
+    font = wezterm.font("ViktorMono NF"),
+  },
+  {
+    italic = true,
+    intensity = "Bold",
+    font = wezterm.font("ViktorMono NF"),
+  },
+}
 
 config.font_size = 12.0
 config.enable_wayland = true
