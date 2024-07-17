a138eea5ea0b6d52bf9fe7a2ff2a0698706f0057
Marco Kellershoff <marco@kellershoff.net>
2024-07-17 10:39:26 +0200

    fix(path): add $HOME/.bin to path

--- configurations/zsh/zsh/sourcing/path.zsh
+++ configurations/zsh/zsh/sourcing/path.zsh
@@ -5,7 +5,7 @@ export PATH=$HOME/.cargo/bin:$PATH
 export GOPATH=$HOME/go
 
 # If you come from bash you might have to change your $PATH.
-export PATH=$HOME/bin:/usr/local/bin:/usr/local/go/bin:$GOPATH/bin:$PATH
+export PATH=$HOME/.bin:/usr/local/bin:/usr/local/go/bin:$GOPATH/bin:$PATH
 
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
