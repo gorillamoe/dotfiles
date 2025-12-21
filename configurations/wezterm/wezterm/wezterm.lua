d61bc3a1394b20abd67aa17ef4778d1ae5151a06
Marco Kellershoff <marco@mistweaverco.com>
2025-12-22 00:21:41 +0100

    feat(wayland): startup gui size

--- configurations/wezterm/wezterm/wezterm.lua
+++ configurations/wezterm/wezterm/wezterm.lua
@@ -46,8 +46,46 @@ tabline.setup({
 tabline.apply_to_config(config)
 
 -- misc settings --
--- prevents crashes with scaling
-config.enable_wayland = false
+
+-- set a fixed dpi to avoid scaling issues and crashes on wayland
+-- see: https://github.com/wezterm/wezterm/issues/5263#issuecomment-3189609408
+config.dpi = 96.0
+
+-- this can crash on wayland without manually setting dpi
+config.enable_wayland = true
+
+-- trickery to get window to a desired size on launch
+-- this is some voodoo for wayland
+wezterm.on("gui-attached", function()
+  local active_screen = wezterm.gui.screens().active
+  local ypx = math.floor(active_screen.height * 0.8)
+  -- don't go too wide for typical monitor aspect ratios
+  -- assume 16:9 max
+  local xpx = math.floor((ypx * 16) / 9)
+  local workspace = wezterm.mux.get_active_workspace()
+  for _, window in ipairs(wezterm.mux.all_windows()) do
+    if window:get_workspace() == workspace then
+      local timeout = os.clock() + 2.0 -- 2 second limit
+      while os.clock() < timeout do
+        local gui_win = window:gui_window()
+        gui_win:set_inner_size(xpx, ypx)
+        -- required exactly at this point in time
+        -- otherwise wezterm reports back the correct size,
+        -- but the window is not actually that size yet
+        -- You can't put that at the end of the loop either, it has to be
+        -- immediately after setting the size
+        wezterm.sleep_ms(100)
+        local dims = gui_win:get_dimensions()
+        -- might be off by a pixel or two, so allow some wiggle room
+        if (math.abs(dims.pixel_width - xpx) <= 2) and (math.abs(dims.pixel_height - ypx) <= 2) then
+          break
+        end
+      end
+    end
+  end
+end)
+
+-- set initial window size
 config.initial_cols = 80
 config.initial_rows = 25
 
