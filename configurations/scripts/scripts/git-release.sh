065934096f830f012ba7ceeb4844562597fb18d5
Marco Kellershoff <marco@mistweaverco.com>
2025-12-25 02:28:14 +0100

    feat(git-release): script got colorful

--- configurations/scripts/scripts/git-release.sh
+++ configurations/scripts/scripts/git-release.sh
@@ -9,16 +9,30 @@
 # If there are no tags, it will create v0.1.0 for minor,
 # v0.0.1 for patch and v1.0.0 for major
 # It will also push the tag to the remote
-# Usage: git-release.sh ["patch"|"minor"|"major"] [<create release message boolean>]
+# Usage: git-release.sh [patch|bug|bugfix|fix|minor|feat|feature|major|break|breaking] [<create release message boolean>]
 # Defaults to patch if no argument is given
 
 RELEASE_TYPE="$1"
 CREATE_RELEASE_MESSAGE="$2"
 LATEST_TAG="v0.0.0"
 NEW_TAG=""
+COLORIZED_NEW_TAG=""
 CONFIRM_TAG_CREATION="n"
 CONFIRM_TAG_PUSH="n"
 
+colorize() {
+  local color_code
+  case "$1" in
+    red) color_code="\033[0;31m";;
+    green) color_code="\033[0;32m";;
+    yellow) color_code="\033[0;33m";;
+    blue) color_code="\033[0;34m";;
+    magenta) color_code="\033[0;35m";;
+    cyan) color_code="\033[0;36m";;
+    *) color_code="\033[0m";;
+  esac
+  echo -e "${color_code}$2\033[0m"
+}
 
 set_latest_tag() {
   local lt
@@ -35,6 +49,22 @@ set_latest_tag() {
   fi
 }
 
+get_colorized_part_tag() {
+  IFS='.' read -r -a parts <<< "${LATEST_TAG#v}"
+  MAJOR=${parts[0]};
+  MINOR=${parts[1]};
+  PATCH=${parts[2]};
+
+  case "$1" in
+    major) echo "$(colorize magenta "v$MAJOR").$MINOR.$PATCH";;
+    minor) echo "v$MAJOR.$(colorize magenta "$MINOR").$PATCH";;
+    patch) echo "v$MAJOR.$MINOR.$(colorize magenta "$PATCH")";;
+    *)
+      echo "Usage: get_colorized_part_tag [major|minor|patch]"
+      return 1;;
+  esac
+}
+
 
 set_new_tag() {
   IFS='.' read -r -a parts <<< "${LATEST_TAG#v}"
@@ -44,30 +74,33 @@ set_new_tag() {
 
   case "$RELEASE_TYPE" in
     major|break|breaking)
-      NEW_TAG="v$((MAJOR + 1)).0.0";;
+      NEW_TAG="v$((MAJOR + 1)).0.0";
+      COLORIZED_NEW_TAG=$(get_colorized_part_tag "major");;
     minor|feat|feature)
-    NEW_TAG="v$MAJOR.$((MINOR + 1)).0";;
+      NEW_TAG="v$MAJOR.$((MINOR + 1)).0";
+      COLORIZED_NEW_TAG=$(get_colorized_part_tag "minor");;
     patch|bug|bugfix|fix|"")
-      NEW_TAG="v$MAJOR.$MINOR.$((PATCH + 1))";;
+      NEW_TAG="v$MAJOR.$MINOR.$((PATCH + 1))";
+      COLORIZED_NEW_TAG=$(get_colorized_part_tag "patch");;
     *)
-      echo "Usage: git release [patch|minor|major] [<create release message boolean>]"
+      echo "Usage: git release [patch|bug|bugfix|fix|minor|feat|feature|major|break|breaking] [<create release message boolean>]"
       return 1;;
   esac
 }
 
 prompt_for_confirmation() {
-  echo -ne "Create tag $NEW_TAG? (y/n) "
+  echo -ne "Create tag $COLORIZED_NEW_TAG? (y/n) "
   read -r -n 1 CONFIRM_TAG_CREATION
   echo
 
   if [[ "$CONFIRM_TAG_CREATION" != "y" ]]; then
-    echo "Tag creation aborted."
+    colorize red "Tag creation aborted."
     exit 0
   fi
 
   if [[ -n "$CREATE_RELEASE_MESSAGE" ]]; then
     if ! git tag -a "$NEW_TAG" -e; then
-      echo "Tag creation aborted."
+      colorize red "Tag creation aborted."
       exit 1
     fi
   else
@@ -80,9 +113,9 @@ prompt_for_confirmation() {
 
   if [[ "$CONFIRM_TAG_PUSH" == "y" ]]; then
     git push origin "$NEW_TAG"
-    echo "Pushed tag $NEW_TAG to remote."
+    colorize green "Pushed tag $COLORIZED_NEW_TAG to remote."
   else
-    echo "Tag $NEW_TAG created locally but not pushed."
+    echo "Tag $COLORIZED_NEW_TAG created locally but not pushed."
   fi
 }
 
