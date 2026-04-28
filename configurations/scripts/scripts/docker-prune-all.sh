ebb71cb60693b9dacb0d82b774b510cf09c777d0
Marco Kellershoff <marco@mistweaverco.com>
2026-04-28 10:28:12 +0200

    fix(scripts): prune all docker images and containers

--- configurations/scripts/scripts/docker-prune-all.sh
+++ configurations/scripts/scripts/docker-prune-all.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# Notifies me
-# Usage: some-long-running-task && notify-me.sh "happy" "Long Running Task Done"
+# Description: Prune all Docker resources and notify the user of the reclaimed space.
+# Usage: docker-prune-all.sh
 
 set -o errexit
 set -o pipefail
 
 OUTPUT=$(docker system prune -a -f --volumes)
 RECLAIMED_SPACE=$(echo "${OUTPUT}" | tail -1)
-notify-me.sh "happy" "Docker Prune All - ${RECLAIMED_SPACE} Reclaimed" "Docker Prune Done"
+notify-me.sh "happy" "${RECLAIMED_SPACE}" "Docker Prune Done"
