c26f6385c13749be696ffbee4d335606c1547766
Marco Kellershoff <marco@mistweaverco.com>
2026-07-08 10:38:36 +0200

    feat(fonts): add departure-mono-nerd-font

--- setup/install-fonts.sh
+++ setup/install-fonts.sh
@@ -1,27 +1,33 @@
 #!/usr/bin/env bash
 
 # Install fonts
-echo "🔤 Installing Nerd Fonts (Fira Code and Victor Mono)"
+echo "🔤 Installing Nerd Fonts (DepartureMono, Fira Code and Victor Mono)"
+echo " - https://departuremono.com"
+echo " - https://github.com/tonsky/FiraCode"
+echo " - https://rubjo.github.io/victor-mono"
 FONTS_INSTALLED=0
 if [ ! -d ~/.local/share/fonts ]; then
   mkdir -p ~/.local/share/fonts
+fi
+if [ -f ~/.local/share/fonts/DepartureMonoNerdFont-Regular.otf ]; then
+  echo "📦 DepartureMonoNerdFont already installed"
+  echo "💡 Skipping DepartureMono installation"
+else
+  cp -p ./configurations/fonts/departure-mono-nerd-font/*.otf ~/.local/share/fonts/
+  FONTS_INSTALLED=1
 fi
 if [ -f ~/.local/share/fonts/FiraCodeNerdFont-Regular.ttf ]; then
   echo "📦 FiraCodeNerdFont already installed"
   echo "💡 Skipping FiraCode installation"
 else
-  cp -p ./configurations/fonts/fira-code-nerd-font/* ~/.local/share/fonts/
-  rm ~/.local/share/fonts/*.md
-  rm ~/.local/share/fonts/LICENSE*
+  cp -p ./configurations/fonts/fira-code-nerd-font/*.ttf ~/.local/share/fonts/
   FONTS_INSTALLED=1
 fi
 if [ -f ~/.local/share/fonts/VictorMonoNerdFont-Regular.ttf ]; then
   echo "📦 VictorMonoNerdFont already installed"
   echo "💡 Skipping VictorMono installation"
 else
-  cp -p ./configurations/fonts/victor-mono-nerd-font/* ~/.local/share/fonts/
-  rm ~/.local/share/fonts/*.md
-  rm ~/.local/share/fonts/LICENSE*
+  cp -p ./configurations/fonts/victor-mono-nerd-font/*.ttf ~/.local/share/fonts/
   FONTS_INSTALLED=1
 fi
 if [ $FONTS_INSTALLED -eq 1 ]; then
